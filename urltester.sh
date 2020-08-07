4ea62e7720e78248945d4c79f41fd3b0c3661811
kodachios <manuela.hellsberger@gmail.com>
2020-08-07 10:37:34 +0200

    Upload v0.1

---
+++ urltester.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+# Simple URL Test Script
+
+# Colors
+
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+CYAN="\e[36m"
+BOLD="\e[1m"
+MAGENTA="\e[35m"
+
+# Variables
+
+file=""
+path=$(pwd)
+useragent="User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
+
+# Banner
+
+clear
+echo -e "${BOLD}${GREEN}"
+figlet UrlTester
+echo -e "			${MAGENTA}by @TheMasterCH"
+echo -e "                                   ${BLUE}v0.2"
+echo ""
+echo -e "${RED}[!] ATLEAST NOT ALL LINKS WITH PATHS ARE WORKING"
+echo ""
+echo ""
+# File Checker
+
+file_checker(){
+  if [ -f bad_con.txt ]; then
+    rm bad_con.txt
+  fi
+  if [ -f dead.txt ]; then
+    rm dead.txt
+  fi
+  if [ -f live.txt ]; then
+    rm live.txt
+  fi
+  if [ -f ddos_guard.txt ]; then
+    rm ddos_guard.txt
+  fi
+  if [ -f != urls.txt ]; then
+    touch urls.txt
+  fi 
+}
+
+file_checker
+
+# Listing All Files (For Check)
+
+sleep 0.5
+echo -e "${GREEN}[*] ${BLUE}Listing files."
+sleep 0.5
+echo -e "${CYAN}"
+ls *.txt
+echo ""
+echo -ne "${YELLOW}[*] ${BLUE}Select file(${RED}urls.txt${BLUE}):${CYAN} "
+read file
+echo
+
+# Configuration
+
+echo -e "${GREEN}[i] ${BLUE}Loading Configuration"
+sleep 0.4
+echo ""
+echo -e "${GREEN}[*] ${BLUE}File selected -: \n${YELLOW}$file\n"
+echo -e "${GREEN}[*] ${BLUE}Useragent selected -: \n${YELLOW}$useragent\n"
+echo -e "${GREEN}[*] ${BLUE}Path -: \n${YELLOW}$path\n"
+file="$file"
+
+cat $file | egrep -o 'https?://[^ ]+' > urls.txt
+file="urls.txt"
+
+# Start Tester
+
+echo -ne "${GREEN}[i] ${BLUE}Press ${RED}[ENTER] ${BLUE}To Start"
+read continue
+echo ""
+echo ""
+while IFS= read -r line
+do
+  code=$(curl -Isk --max-time 10 -H "$useragent" $line | head -n1 | awk '{print $2}')
+  if [[ "$code" == 200 ]]; then
+    echo -e "${GREEN}[+] ${BLUE}Website ${GREEN}Live ${BLUE}: ${CYAN}$line "
+    if [ -f live.txt ]; then
+      echo $line >> live.txt
+    else
+      touch live.txt
+      echo $line >> live.txt
+    fi
+
+  elif [[ "$code" == "" ]]; then
+    html=$(curl -sk --max-time 10 -H "$useragent" $line | head -n1)
+    if [[ "$html" == "<!DOCTYPE HTML>" ]]; then
+      echo -e "${YELLOW}[-] ${BLUE}Website ${YELLOW}Live But Bad Connection${BLUE} : ${CYAN}$line "
+      if [ -f bad_con.txt ]; then
+        echo $line >> bad_con.txt
+      else
+        touch bad_con.txt
+        echo $line >> bad_con.txt
+      fi
+    fi
+
+  elif [[ "$code" == 403 ]]; then
+    test=$(curl -Isk --max-time 10 -H "$useragent" $line | head -n2 | tail -1 | awk '{print $2}')
+    if [[ "$test" == "ddos-guard" ]]; then
+      echo -e "${MAGENTA}[-] ${BLUE}Live But Detected ${MAGENTA} DDoS Guard${BLUE} : ${CYAN}$line "
+      if [ -f ddos_guard.txt ]; then
+        echo $line >> ddos_guard.txt
+      else
+        touch bad_con.txt
+        echo $line >> ddos_guard.txt
+      fi
+    fi
+  elif [[ "$code" == 503 ]]; then
+    echo -e "${YELLOW}[-] ${BLUE}Website ${YELLOW}Live But Bad Connection${BLUE} : ${CYAN}$line "
+    if [ -f bad_con.txt ]; then
+      echo $line >> bad_con.txt
+    else
+      touch bad_con.txt
+      echo $line >> bad_con.txt
+    fi
+
+  else
+    echo -e "${RED}[!] ${BLUE}Website ${RED}Dead ${BLUE}: ${CYAN}$line "
+    if [ -f dead.txt ]; then
+      echo $line >> dead.txt
+    else
+      touch dead.txt
+      echo $line >> dead.txt
+    fi
+  fi
+done < "$file"
+
+rm $file
+
+# End
+
+echo ""
+echo -ne "${GREEN}[i] ${BLUE}Press [ENTER] To Exit"
+read exit
